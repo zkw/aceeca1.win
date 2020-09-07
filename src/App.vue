7335f05ea3cfcba5b01d35f7c63ce00bdba7d6d3
Kunwei Zhang <aceeca.135531@gmail.com>
2020-09-07 17:28:40 +0800

    晴

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template lang="pug">
 #app
-  #nav
-    router-link(to="/") Home
-    |
-    | |
-    |
-    router-link(to="/about") About
+  b-navbar(toggleable="lg" type="dark" variant="dark")
+    b-navbar-brand(to="/") 张昆玮的博客
+    b-navbar-toggle(target="nav-collapse")
+    b-collapse#nav-collapse(is-nav)
+      b-navbar-nav
+        b-nav-item(to="/about") 关于我
+        b-nav-item-dropdown(text="特色内容")
+      b-navbar-nav.ml-auto
+        b-nav-item-dropdown(:text="user" right)
+          b-dropdown-item 登出
   router-view
 </template>
