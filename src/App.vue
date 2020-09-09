9f390873503976304cd32a8bc08c8b6bfd12225a
Kunwei Zhang <aceeca.135531@gmail.com>
2020-09-10 07:34:38 +0800

    晴

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,10 @@
       b-navbar-nav.ml-auto
         b-nav-item-dropdown(:text="getUser()" right)
           b-dropdown-item(v-if="!user" v-b-toggle.login @click="requireToken") 微信登录
-          b-dropdown-item(v-if="user") 修改昵称
+          b-dropdown-item(v-if="user" v-b-toggle.set-nick) 修改昵称
           b-dropdown-item(v-if="user" @click="logout") 登出
+  b-alert(:show="successCountDown" @dismiss-count-down="successCountDownChanged") 成功
+  b-alert(variant="danger" :show="failureCountDown" @dismiss-count-down="failureCountDownChanged") 失败
   b-sidebar#login(lazy bg-variant="dark" text-variant="light" right)
     b-card.mt-3(bg-variant="info")
       | 请微信关注“张昆玮”公众号，
@@ -20,8 +22,12 @@
       h1.text-center {{ token }}
       b-button.m-2(variant="primary" v-b-toggle.login @click="login") 我已发送，继续登录
       b-button.m-2(variant="danger" v-b-toggle.login) 取消
-  b-alert(:show="successCountDown" @dismiss-count-down="successCountDownChanged") 登录成功
-  b-alert(variant="danger" :show="failureCountDown" @dismiss-count-down="failureCountDownChanged") 登录失败
+  b-sidebar#set-nick(lazy bg-variant="dark" text-variant="light" right)
+    b-card.mt-3(bg-variant="info")
+      .m-2 当前昵称: {{ user }}
+      b-form-input.m-2(v-model="nick" placeholder="新的昵称")
+      b-button.m-2(variant="primary" v-b-toggle.set-nick @click="setNick") 修改
+      b-button.m-2(variant="danger" v-b-toggle.set-nick) 取消
   router-view
 </template>
 
@@ -30,6 +36,7 @@ export default
   data: ->
     successCountDown: 0
     failureCountDown: 0
+    nick: null
     token: null
     user: null
   methods:
@@ -39,7 +46,7 @@ export default
     login: ->
       ajax = await @axios.get('/ajax/user-login-2')
       @user = ajax.data
-      if @user then @successCountDown = 3 else @failureCountDown = 3
+      if @user then @successCountDown = 2 else @failureCountDown = 2
     logout: ->
       ajax = await @axios.get('/ajax/user-logout')
       @user = null
@@ -54,5 +61,8 @@ export default
       @successCountDown = dismissCountDown
     failureCountDownChanged: (dismissCountDown) ->
       @failureCountDown = dismissCountDown
+    setNick: ->
+      ajax = await @axios.get('/ajax/user-set-nick', nick: @nick)
+      if ajax.data then @successCountDown = 2 else @failureCountDown = 2
   mounted: -> @requireUser()
 </script>
