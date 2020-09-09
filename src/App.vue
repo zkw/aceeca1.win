645a286fd06634f8e963ce545310de573d7f5278
Kunwei Zhang <aceeca.135531@gmail.com>
2020-09-09 12:07:46 +0800

    晴

--- src/App.vue
+++ src/App.vue
@@ -18,14 +18,18 @@
       br
       | 并发送验证码：
       h1.text-center {{ getToken() }}
-      b-button.m-2(variant="primary" @click="login") 我已发送，继续登录
+      b-button.m-2(variant="primary" v-b-toggle.login @click="login") 我已发送，继续登录
       b-button.m-2(variant="danger" v-b-toggle.login) 取消
+  b-alert(:show="successCountDown" @dismiss-count-down="successCountDownChanged") 登录成功
+  b-alert(variant="danger" :show="failureCountDown" @dismiss-count-down="failureCountDownChanged") 登录失败
   router-view
 </template>
 
 <script lang="coffee">
 export default
   data: ->
+    successCountDown: 0
+    failureCountDown: 0
     token: null
     user: null
   methods:
@@ -35,11 +39,17 @@ export default
     login: ->
       ajax = await @axios.get('https://wx.aceeca1.win/ajax/user-login-2')
       @user = ajax.data
+      if @user then @successCountDown = 3 else @failureCountDown = 3
     requireToken: ->
       ajax = await @axios.get('https://wx.aceeca1.win/ajax/user-login-1')
       @token = ajax.data
     requireUser: ->
       ajax = await @axios.get('https://wx.aceeca1.win/ajax/user-status')
+      console.log(ajax.data)
       @user = ajax.data
+    successCountDownChanged: (dismissCountDown) ->
+      @successCountDown = dismissCountDown
+    failureCountDownChanged: (dismissCountDown) ->
+      @failureCountDown = dismissCountDown
   mounted: -> @requireUser()
 </script>
